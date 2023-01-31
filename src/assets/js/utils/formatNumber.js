0ee410b405788e0271f7deb2b3cab138fb7135b7
Adam-Kolt <adamkoltuniuk@gmail.com>
2023-01-31 16:58:14 -0500

    Optimized Team Display code

---
+++ src/assets/js/utils/formatNumber.js
@@ -0,0 +1,3 @@
+export function round2Two(number) {
+    return +(Math.round(number + "e+2")  + "e-2");
+}
--- src/assets/js/widgets/Data/teamDisplay.js
+++ src/assets/js/widgets/Data/teamDisplay.js
@@ -1,80 +1,59 @@
+function round2Two(number) {
+  return +(Math.round(number + "e+2")  + "e-2");
+}
+
 function fillRawTeamData(event) {
     let data = event.detail.scout_data;
     let teams = Object.values(data);
-
+    
     let teamEntryList = document.querySelector(".data-team-raw-display");
     teams.forEach(team => {
       let teamEntry = document.createElement("div");
-      teamEntry.classList.add("team-entry");
-      
-      let label = document.createElement("div");
-      label.classList.add("label");
-      label.textContent = team.team_number;
-      teamEntry.appendChild(label);
-
-      let name = document.createElement("div");
-      name.classList.add("name")
-      if (team.tba_data.nickname) {name.textContent = team.tba_data.nickname};
-      teamEntry.appendChild(name);
-
-      if (team.summary) {
-        let coneTable = document.createElement("table");
-        coneTable.classList.add("cone-table");
-        
-        coneTable.innerHTML = `
-        <caption><object class="caption-icon" data="./assets/svg/cone.svg" type=""></object></caption>
-        <tr>
-          <th>Low</th>
-          <th>Medium</th>
-          <th>High</th>
-        </tr>
-        <tr>
-          <td>${team.summary.avg_cone_low}</td>
-          <td>${team.summary.avg_cone_med}</td>
-          <td>${team.summary.avg_cone_high}</td>
-        </tr>
-        `
-
-        teamEntry.appendChild(coneTable);
-        
-        // May or may not be able to just inject any html code by setting team summary variables...probably ok...
-        let cubeTable = document.createElement("table");
-        cubeTable.classList.add("cube-table");
-        cubeTable.innerHTML = `
-        <caption><object class="caption-icon" data="./assets/svg/cube.svg" type=""></object></caption>
-        <tr>
-          <th>Low</th>
-          <th>Medium</th>
-          <th>High</th>
-        </tr>
-        <tr>
-          <td>${team.summary.avg_cube_low}</td>
-          <td>${team.summary.avg_cube_med}</td>
-          <td>${team.summary.avg_cube_high}</td>
-        </tr>`
-
-        teamEntry.appendChild(cubeTable);
-
-
-        let stationTable = document.createElement("table");
-        stationTable.classList.add("station-table");
-        stationTable.innerHTML = `
-        <caption>Charge Station</caption>
-        <tr>
-          <th>Can Balance</th>
-          <th>Docked %</th>
-          <th>Engaged %</th>
-        </tr>
-        <tr>
-          <td>None</td>
-          <td>None</td>
-          <td>None</td>
-        </tr>
-        `
-
-        teamEntry.appendChild(stationTable);
-
-      }
+      teamEntry.classList.add("team-entry")
+      // May be unwise to use innerHtml as it will execute any html put in the variables...but its probably fine...
+      teamEntry.innerHTML = `
+      <div class="label">${team.team_number}</div>
+              <div class="team-name">${team.tba_data.nickname}</div>
+              <table class="cone-table">
+                <caption><object class="caption-icon" data="./assets/svg/cone.svg" type=""></object></caption>
+                <tr>
+                  <th>Low</th>
+                  <th>Medium</th>
+                  <th>High</th>
+                </tr>
+                <tr>
+                  <td>${round2Two(team.summary.avg_cone_low)}</td>
+                  <td>${round2Two(team.summary.avg_cone_med)}</td>
+                  <td>${round2Two(team.summary.avg_cone_high)}</td>
+                </tr>
+              </table>
+              <table class="cube-table">
+                <caption><object class="caption-icon" data="./assets/svg/cube.svg" type=""></object></caption>
+                <tr>
+                  <th>Low</th>
+                  <th>Medium</th>
+                  <th>High</th>
+                </tr>
+                <tr>
+                  <td>${round2Two(team.summary.avg_cube_low)}</td>
+                  <td>${round2Two(team.summary.avg_cube_med)}</td>
+                  <td>${round2Two(team.summary.avg_cube_high)}</td>
+                </tr>
+              </table>
+              <table class="station-table">
+                <caption>Charge Station</caption>
+                <tr>
+                  <th>Can Balance</th>
+                  <th>Docked %</th>
+                  <th>Engaged %</th>
+                </tr>
+                <tr>
+                  <td>None</td>
+                  <td>None</td>
+                  <td>None</td>
+                </tr>
+              </table>
+      `
 
       teamEntryList.appendChild(teamEntry);
 
