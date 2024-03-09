0c1e3f27e51764d19ce3980faea1931e3b61336d
cjayamanne <162512395+cjayamanne@users.noreply.github.com>
2024-03-09 16:43:08 -0500

    Update matchOverviewDisplay.js

--- src/assets/js/widgets/Match/matchOverviewDisplay.js
+++ src/assets/js/widgets/Match/matchOverviewDisplay.js
@@ -8,63 +8,59 @@ function getValueStroke(value) {
   
   function updateTeamCard(event, data) {
       let teamCard = document.getElementById(event.srcElement.dataset.cardId);
+      
       let teamNumber = event.srcElement.value;
       if (!teamNumber) return;
+
       let team = data[teamNumber];
       // TODO: Implement Charge Station Stats
       // TODO: Add Progress/Circle Percentage Meters for Charge Station Stats
       // TODO: Add Export to Pretty Image/Banner of the Match
       
       teamCard.innerHTML = `
-      <div class="label">${team.team_number}</div>
+      <div class="label">${team.teamNumber}</div>
                     <div class="team-name">${(team.tba_data) ? team.tba_data.nickname:"Team"} (${team.match_data.length} ${(team.match_data.length == 1) ? "Entry":"Entries" })</div>
                     <div class="cones-row">
-                    <object class="caption-icon" data="./assets/svg/cone.svg" type=""></object>
+                    <object class="caption-icon" data="" type=""></object>
                       <table class="cone-table">
                         <tr>
-                          <th>Low</th>
-                          <th>Medium</th>
-                          <th>High</th>
+                          <th>Auton Amp Avg</th>
+                          <th>Auto Speaker Avg</th>
                         </tr>
                         <tr>
-                          <td>${round2Two(team.summary.avg_cone_low)}</td>
-                          <td>${round2Two(team.summary.avg_cone_med)}</td>
-                          <td>${round2Two(team.summary.avg_cone_high)}</td>
+                          <td>${round2Two(team.summary.auton_amp_avg)}</td>
+                          <td>${round2Two(team.summary.auton_speaker_avg)}</td>
                         </tr>
                       </table>
                     </div>
                     <div class="cubes-row">
-                      <object class="caption-icon" data="./assets/svg/cube.svg" type=""></object>
+                      <object class="caption-icon" data="" type=""></object>
                       <table class="cube-table">
                         <tr>
-                          <th>Low</th>
-                          <th>Medium</th>
-                          <th>High</th>
+                          <th>Amp Avg</th>
+                          <th>Speaker Avg</th>
                         </tr>
                         <tr>
-                          <td>${round2Two(team.summary.avg_cube_low)}</td>
-                          <td>${round2Two(team.summary.avg_cube_med)}</td>
-                          <td>${round2Two(team.summary.avg_cube_high)}</td>
+                          <td>${round2Two(team.summary.total_amp_avg)}</td>
+                          <td>${round2Two(team.summary.total_speaker_avg)}</td>
                         </tr>
                       </table>
                     </div>
   
                 <table class="station-table">
                   <tr>
-                    <th class="station-text">Balance</th>
-                    <th class="station-text">Dock %</th>
-                    <th class="station-text">Engage %</th>
+                    <th class="station-text">Endgame Trap</th>
+                    <th class="station-text">Endgame Climb</th>
                   </tr>
                   <tr>
-                    <td style="text-align:center;">${team.summary.can_balance.toString().toUpperCase()}</td>
-                    <td><div class="percentage-bar ldBar label-center"></div></td>
-                    <td><div class="percentage-bar ldBar label-center"></div></td>
+                    <td style="text-align:center;">${team.summary.points_trap}</td>
+                    <td style="text-align:center;">${team.summary.climb_count}</td>
                   </tr>
                 </table>`
                 
-      let percentageBars = teamCard.getElementsByClassName("percentage-bar");
-      new ldBar(percentageBars[0], { "value": Math.round(team.summary.balance_percentage*100), "stroke": "white", "preset": "circle"});
-      new ldBar(percentageBars[1], { "value": Math.round(team.summary.dock_percentage*100), "stroke": "white", "preset": "circle"});
+//      let percentageBars = teamCard.getElementsByClassName("percentage-bar");
+//      new ldBar(percentageBars[0], { "value": Math.round(team.summary.balance_percentage*100), "stroke": "white", "preset": "circle"});
+//      new ldBar(percentageBars[1], { "value": Math.round(team.summary.dock_percentage*100), "stroke": "white", "preset": "circle"});
   
    }
   
@@ -75,9 +71,7 @@ function getValueStroke(value) {
       let teamOptionsHtml = "<option value='' selected disabled hidden>Team</option>"
       teamNumbers.forEach(teamNumber => {
           
-          
           teamOptionsHtml += `<option value=${teamNumber}>${teamNumber}</option>`
-          
           
       })
   
